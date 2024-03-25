fbdd572f3ac93d22e729241474f3db717db05fa4
MarioPasc <mario.pg02@gmail.com>
2024-03-25 16:14:16 +0100

    Microorganisms specifications

--- src/db_creation.py
+++ src/db_creation.py
@@ -1,5 +1,8 @@
-from typing import List, Optional
+from typing import List, Optional, Dict
 import mysql.connector as mysql
+import random
+import string
+import pandas as pd
 
 class DbCreation:
     
@@ -39,4 +42,59 @@ class DbCreation:
         finally:
             if connection is not None:
                 connection.close()
-        return connection  
+        return connection
+    
+    
+class DataGenerator:
+    def __init__(self, num_samples: int):
+        self.num_samples = num_samples
+        self.table_names: List[str] = ["Patient", "Sample", "Microorganism", "Sample_Microorganism"]
+        self.patient_attributes: List[str] = [
+            "Patient_ID",  # String, format: PAC + 5 digits + 3 letters
+            "Age",         # Integer, range: [0-100]
+            "Birth_Type",  # String, options: ['cesarean', 'natural']
+            "Location",    # String, options: ['Europe', 'Africa', 'North America', 'South America', 'Central Asia', 'East Asia']
+            "Lifestyle",   # String, options: ['Active', 'Sedentary']
+            "Disease"      # String
+            ]
+        self.sample_attributes: List[str] = [
+            "Sample_ID",   # String, format: MUE + 5 digits + 3 letters
+            "Patient_ID",  # String, Foreign Key
+            "Date",        # String, Date
+            "Body_Part",   # String
+            "Sample_Type"  # String
+            ]
+        self.microorganism_attributes: List[str]= [
+            "Microorganism_ID", # String, format: MICRO + 5 digits + 3 letters
+            "Microorganism_Name",
+            "Species",          # String
+            "Kingdom",          # String
+            "FASTA",            # String, format: seq_ + Microorganism ID + .fasta
+            "Seq_length"        # Integer, calculation: 150 * random number [10^6-10^8]
+            ]
+        self.sample_microorganism_attributes: List[str] = [
+            "Sample_ID",        # String, Foreign Key, Primary Key
+            "Microorganism_ID", # String, Foreign Key, Primary Key
+            "qPCR"              # Integer, Copies/mL, random number
+            ]
+        self.all_attributes: List[str] = [
+            "Patient_ID",
+            "Age",
+            "Birth_Type",
+            "Location",
+            "Lifestyle",
+            "Disease",
+            "Sample_ID",
+            "Date",
+            "Body_Part",
+            "Sample_Type",
+            "Microorganism_Name"
+            "Microorganism_ID",
+            "Species",
+            "Kingdom",
+            "FASTA",
+            "Seq_length",
+            "qPCR"
+            ]
+
+
